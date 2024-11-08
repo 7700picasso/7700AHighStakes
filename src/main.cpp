e37930205846c32bc6ef9a7abeb3cfb28b387fd7
7700R <127266781+7700BVRC@users.noreply.github.com>
2024-11-07 22:03:12 -0800

    motor monitor

--- src/main.cpp
+++ src/main.cpp
@@ -50,6 +50,117 @@ void driveBrake() {
 }
 
 
+
+double YOFFSET = 20; //offset for the display
+//Writes a line for the diagnostics of a motor on the Brain
+void MotorDisplay(double y, double curr, double temp)
+{
+	Brain.Screen.setFillColor(transparent);
+	Brain.Screen.printAt(5, YOFFSET + y, "Current: %.1fA", curr);
+	
+	if (curr < 1){
+		Brain.Screen.setFillColor(green);
+    Brain.Screen.drawRectangle(140, YOFFSET + y - 15, 15, 15);
+	} else if(curr >= 1 && curr  <= 2.5) {
+		Brain.Screen.setFillColor(yellow);
+    Brain.Screen.drawRectangle(140, YOFFSET + y - 15, 15, 15);
+	} else {
+		Brain.Screen.setFillColor(red);
+		Brain.Screen.drawRectangle(140, YOFFSET + y - 15, 15, 15);
+	}
+
+	Brain.Screen.setFillColor(transparent);
+	Brain.Screen.printAt(160, YOFFSET + y, "Temp: %.1fC", temp);
+	
+	if (temp < 45){
+		Brain.Screen.setFillColor(green);
+    Brain.Screen.drawRectangle(275, YOFFSET + y - 15, 15, 15);
+	} else if(temp <= 50 && temp  >= 45){
+	
+    Brain.Screen.setFillColor(yellow);
+    Brain.Screen.drawRectangle(275, YOFFSET + y - 15, 15, 15);
+		
+	} else {
+		Brain.Screen.setFillColor(red);
+		Brain.Screen.drawRectangle(275, YOFFSET + y - 15, 15, 15);
+    
+		
+	}
+}
+
+
+//Displays information on the brain
+void Display()
+{
+	double leftFrontCurr = LF.current(amp);
+	double leftFrontTemp = LF.temperature(celsius);
+	double leftBackCurr = LB.current(amp);
+	double leftBackTemp = LB.temperature(celsius);
+	double rightFrontCurr = RF.current(amp);
+	double rightFrontTemp = RF.temperature(celsius);
+	double rightBackCurr = RB.current(amp);
+	double rightBackTemp = RB.temperature(celsius);
+  double intakeTemp = intake.temperature(celsius); 
+  double intakeCurr = intake.current(amp); 
+  double hookTemp = hook.temperature(celsius); 
+  double hookCurr = hook.current(amp); 
+  
+	if (LF.installed()){
+		MotorDisplay(1, leftFrontCurr, leftFrontTemp);
+    Brain.Screen.setFillColor(transparent);
+		Brain.Screen.printAt(300, YOFFSET + 1, "LeftFront");
+	} else {
+		Brain.Screen.printAt(5, YOFFSET + 1, "LeftFront Problem");
+	}
+	
+	
+	if (LB.installed()){
+    
+		MotorDisplay(31, leftBackCurr, leftBackTemp);
+    Brain.Screen.setFillColor(transparent);
+		Brain.Screen.printAt(300, YOFFSET + 31, "LeftBack");
+	} else {
+		Brain.Screen.printAt(5, YOFFSET + 31, "LeftBack Problem");
+	}
+
+
+	if (RF.installed()) {
+   
+		MotorDisplay(61, rightFrontCurr, rightFrontTemp);
+     Brain.Screen.setFillColor(transparent);
+		Brain.Screen.printAt(300, YOFFSET + 61, "RightFront");
+	} else {
+		Brain.Screen.printAt(5, YOFFSET + 61, "RightFront Problem");
+	}
+	
+	
+	if (RB.installed()) {
+    
+		MotorDisplay(91, rightBackCurr, rightBackTemp);
+    Brain.Screen.setFillColor(transparent);
+		Brain.Screen.printAt(300, YOFFSET + 91, "RightBack");
+	} else {
+		Brain.Screen.printAt(5, YOFFSET + 91, "RightBack Problem");
+	}
+  if (intake.installed()) {
+  
+		MotorDisplay(121, intakeCurr, intakeTemp);
+    Brain.Screen.setFillColor(transparent);
+		Brain.Screen.printAt(300, YOFFSET + 121, "intake");
+	} else {
+		Brain.Screen.printAt(5, YOFFSET +121, "Intake Problem");
+	}
+  if (hook.installed()) {
+    
+		MotorDisplay(151, hookCurr, hookTemp);
+    Brain.Screen.setFillColor(transparent);
+		Brain.Screen.printAt(300, YOFFSET + 151, "hook");
+	} else {
+		Brain.Screen.printAt(5, YOFFSET + 151, "Hook Problem");
+	}
+}
+
+
 /*---------------------------------------------------------------------------*/
 
 
@@ -71,8 +182,8 @@ void pre_auton(void) {
 
 void autonomous(void) {
   // ..........................................................................
-  driveRobot(20, 20, 2000); 
-  driveBrake(); 
+  // driveRobot(20, 20, 2000); 
+  // driveBrake(); 
 
   // ..........................................................................
 }
@@ -91,6 +202,7 @@ void usercontrol(void) {
   // User control code here, inside the loop
   while (1) {
     
+    Display(); 
 
     int rspeed = Controller1.Axis2.position(pct); 
     int lspeed = Controller1.Axis3.position(pct);
